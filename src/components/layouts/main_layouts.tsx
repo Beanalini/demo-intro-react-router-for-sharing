9fc6b501fbc3d67699a75225111cc65bc365096e
Beanalini <dat826@gmail.com>
2023-12-13 16:40:55 +0000

    feat: add react router to app

--- src/components/layouts/main_layouts.tsx
+++ src/components/layouts/main_layouts.tsx
@@ -1,1 +1,17 @@
-export {}
+import Header from "../header/header";
+import { Outlet } from "react-router";
+import Footer from "../footer/footer";
+
+const MainLayout: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <main className="p-6" style={{ flex: "1" }}>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
